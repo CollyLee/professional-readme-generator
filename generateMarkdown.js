74e6346c46e17ece7f929fe5a59f8b32fb1adf91
CollyLee <clh5160@gmail.com>
2023-04-11 19:12:15 -0400

    wrote questions and README md template

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  /n
+  /n
+  ## Description /n
+  /n
+  ${description} /n
+  /n
+  #Table of Contents /n
+  /n
+  [Description](#description) /n
+  [Installation](#installation) /n
+  [Usage](#usage) /n
+  [How to Contribute](#How to Contribute) /n
+  /n
+  #Installation /n
+  /n
+  ${installation} /n
+  /n
+  #Usage /n
+  /n
+  ${usage} /n
+  /n
+  #How to Contribute /n
+  /n
+  ${contributions} /n
+  /n
+  If you have any questions or issues about the application, don't hesitate to reach out to me at ${contactInfo}, or on GitHub at https://github.com/${gitName}
+
 
 `;
 }
--- index.js
+++ index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Hi there! To get started, first give me the title of your repo.',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Great! Can you give me a brief description of the project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'If the product is already deployed, go ahead and give the URL here.',
+        name: 'deployedURL',
+    },
+    {
+        type: 'input',
+        message: 'What instructions would you give to users to install the application?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Next, tell them what they need to know in order to use the application',
+        name: 'instructions',
+    },
+    {
+        type: 'input',
+        message: "If you've got a screenshot, provide the path to that here.",
+        name: 'screenshotPath',
+    },
+    {
+        type: 'input',
+        message: "Let the users know how they can contribute to your project. Just type N/A if you don't want them contributing.",
+        name: 'contributions',
+    },
+    {
+        type: 'input',
+        message: "Can you please give me your email address. That way users can reach out to you if needed.",
+        name: 'contactInfo',
+    },
+    {
+        type: 'input',
+        message: "Give me your user name on GitHub.",
+        name: 'gitName',
+    },
+    {
+        type: 'list',
+        message: "Almost done! Select which license this app is covered under.",
+        name: 'license',
+        choices: "'MIT', 'Boose Software 1.0', 'Creative Commons Zero v1.0 Universal', 'N/A'"
+    },
+]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    .then((data) => console.log(data)
+    );
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (err) =>
+    error ? console.error(err) : console.log("Your README is done! Check your root folder for the file.");
+)}
 
 // Function call to initialize app
-init();
+// init() {
+//     inquirer.prompt(questions)
+
+//     .then ((response) {
+//         console.log(response)
+
+//     })
+// }
