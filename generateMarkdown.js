1bffb6ed3db4f25cc8f2b5d24d6b17667becfb35
CollyLee <clh5160@gmail.com>
2023-04-12 19:25:35 -0400

    finalized markdown template, added screenshot

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -8,39 +8,53 @@ function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {}
+//   if (license === "N/A") {
+//     return "";
+//   } else { return `#License /n /n The license used for this application is ${license}` }
+// }
 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ## Description /n
-  
-  ${data.description} 
-  
-  #Table of Contents 
-  
-  [Description](#description) 
-  [Installation](#installation) 
-  [Usage](#usage) 
-  [How to Contribute](#How to Contribute) 
-  
-  #Installation 
-  
-  ${data.installation} 
-  
-  #Usage 
+## Description
 
-  ${data.usage} 
-  
-  #How to Contribute 
-  
-  ${data.contributions} 
-  
-  If you have any questions or issues about the application, don't hesitate to reach out to me at ${data.contactInfo}, or on GitHub at https://github.com/${data.gitName}
+${data.description} 
+
+# Table of Contents 
+
+- [Description](#description)
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Contributions](#Contributions)
+
+# Installation 
+
+${data.installation} 
+
+# Usage 
+
+${data.instructions} 
+
+![project screenshot](${data.screenshotPath})
+
+# License
+
+This application is covered under the ${data.license} license.
+
+# Contributions 
+
+${data.contributions}. Contributions can be emailed to me at ${data.contactInfo}.
+
+If you have any questions or issues about the application, don't hesitate to reach out to me at ${data.contactInfo}, or on GitHub at https://github.com/${data.gitName}
 
 `;
+
 }
 
 module.exports = generateMarkdown;
